f01e2cd941cfc47899e4077b8c37f1ed3b18c91c
Ryan Hinkle <hinkle.ryan97@gmail.com>
2021-12-28 15:09:00 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Ryan Hinkle';
+
+let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+let shortGreeting = 'Welcome, ' + 'R' + '!';
